82bf13e1dee8d7b243b3c3fa8e2e57a50bafd820
Dan Carley <dan.carley@gmail.com>
2013-07-01 10:06:25 +0100

    [#52118601] Manage /etc/hosts as a file resource
    Convert all `/etc/hosts` entries from `host{}` resources to a single
    `file{}` resource. This will ensure ensure that our FQDN entry always
    appears in the correct position and that `hostname -f` will work as
    intended.
    
    As a result, we can no longer support the `purge` param. It is implied (and
    documented) that this will remove any existing entries. It won't be possible
    to use this in conjunction with any `host{}` resources. For the opinionated
    reason that you should be using DNS instead.
    
    Per the previous commit, this also introduces one new entry for
    `ip6-allhosts`. This is from Ubuntu stock.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,50 +2,19 @@
 #
 # Manage standard /etc/hosts entries:
 #
+# - Self hostname and FQDN (where available).
 # - IPv4 loopback.
 # - IPv6 lookback and multicast.
 #
-# == Parameters:
+# This manages `hosts(5)` as a standard `file{}` resource. As a result, any
+# stray entries will be removed.
 #
-# [*purge*]
-#   Purge unmanaged hosts entries. This is recommended.
-#   Default: true
-#
-class hosts(
-  $purge = true
-) {
-  resources { 'host':
-    purge => $purge,
-  }
-
-  host { $::fqdn:
-    ensure        => present,
-    ip            => '127.0.1.1',
-    host_aliases  => $::hostname,
-  }
-
-  host {
-    'localhost':
-      ensure        => present,
-      ip            => '127.0.0.1';
-    'ip6-localhost':
-      ensure        => present,
-      ip            => '::1',
-      host_aliases  => [
-        'ip6-loopback',
-        'localhost',
-      ];
-    'ip6-localnet':
-      ensure        => present,
-      ip            => 'fe00::0';
-    'ip6-mcastprefix':
-      ensure        => present,
-      ip            => 'ff00::0';
-    'ip6-allnodes':
-      ensure        => present,
-      ip            => 'ff02::1';
-    'ip6-allrouters':
-      ensure        => present,
-      ip            => 'ff02::2';
+class hosts {
+  file { '/etc/hosts':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('hosts/etc/hosts.erb'),
   }
 }
